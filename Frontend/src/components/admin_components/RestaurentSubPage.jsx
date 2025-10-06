5c85d0d2b14368e32dc3001880a1b3e64c880049
nitin833 <nitin.vempati@gmail.com>
2025-10-06 18:32:04 +0530

    added new jsx components in admin_components

---
+++ Frontend/src/components/admin_components/RestaurentSubPage.jsx
@@ -0,0 +1,11 @@
+export function RestaurantSubPage(){
+
+
+return (
+
+
+    <></>
+);
+
+
+}
--- Frontend/src/pages/AdminPage.jsx
+++ Frontend/src/pages/AdminPage.jsx
@@ -3,19 +3,35 @@ import { isLogin } from "../util/auth";
 import User from '../components/admin_components/User'
 import Restaurant from "../components/admin_components/Restaurant";
 import Requests from "../components/admin_components/Requests";
+import { AdminDashBoard } from "../components/admin_components/AdminDashBoard";
+import { Settings } from "../components/admin_components/Settings";
+import { RestaurantSubPage } from "../components/admin_components/RestaurentSubPage";
+import { useState } from "react";
 
 export function AdminPage(){
 
 
     let data = useLoaderData();
+
+    const [subPage,setSubPage] = useState("dashboard");
+
+
+
 return (
     
 
-    <>
     
-    this is admin page 
- 
-   
+    <>
+
+        <h5 onClick={()=>{setSubPage("dashboard")}}>dashboard</h5>
+        <h5 onClick={()=>{setSubPage("user")}}>user management</h5>
+        <h5 onClick={()=>{setSubPage("restaurant")}}>restaurant management</h5>
+        <h5 onClick={()=>{setSubPage("settings")}}>setting</h5>
+
+        {subPage=="dashboard" && <AdminDashBoard></AdminDashBoard>}
+        {subPage=="user" && <User></User>}
+        {subPage=="restaurant" && <RestaurantSubPage></RestaurantSubPage>}
+        {subPage=="settings" && <Settings></Settings>}
      
 
     
